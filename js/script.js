8f46d2852ccf55afeaca2ee96ed6f40bfb4955e1
Maksim <maksim.karpuk13910@gmail.com>
2022-04-04 15:56:59 +0300

    feature/4

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("form__footer-time").innerHTML = Date();
+function submit(e) {
+  e.preventDefault();
+}
+const form = document.getElementById("form");
+form.addEventListener("submit", submit);
+
+const checkboxies = document.querySelector(".form__addservice-checkbox");
+function toggleAddService(){
+    checkboxies.classList.toggle("isHidden")
+}
+//addService.addEventListener("click", toggleAddService);
+
+
