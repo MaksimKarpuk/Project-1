7a1b989073a29d976729908d87cd01cf005d5611
Maksim <maksim.karpuk13910@gmail.com>
2022-04-05 10:22:13 +0300

    feature/5

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ document.getElementById("form__footer-time").innerHTML = Date();
 function submit(e) {
   e.preventDefault();
 }
-const form = document.getElementById("form");
+
+
+/*const form = document.getElementById("form");
 form.addEventListener("submit", submit);
 
 
@@ -11,6 +13,17 @@ const checkboxies = document.querySelector(".form__addservice-checkbox");
 function toggleAddService(){
     checkboxies.classList.toggle("isHidden")
 }
-//addService.addEventListener("click", toggleAddService);
-
+addService.addEventListener("click", toggleAddService);
+*/
+const hiddenMenu = document.querySelector(".banner__menu-hidden");
+const burgerMenuOff = document.querySelector(".banner__hamburger");
+function toggleHiddenMenu (){
+    hiddenMenu.classList.toggle("show")
+    burgerMenuOff.style.display = "none"
+}
+const burgerMenuOn = document.querySelector (".banner__menu-hidden");
+function hiddenMenuOff() {
+    burgerMenuOn.classList.toggle("isHidden")
+    burgerMenuOff.style.display = "block"
+}
 
