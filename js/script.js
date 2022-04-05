92e7e4c72ff21b8e40305e69c1ed6f0e9f68ec43
Maksim <maksim.karpuk13910@gmail.com>
2022-04-05 17:22:46 +0300

    feature/5

--- js/script.js
+++ js/script.js
@@ -3,11 +3,16 @@ function submit(e) {
   e.preventDefault();
 }
 
+/*const headerMenu = document.querySelector(".header__menu")
+const link = document.createElement ("a")
+link.className = "header__link"
+link.innerHTML = "О нас"
+headerMenu.insertBefore (link, headerMenu.firstChild)
+link.remove()
+*/
 
 /*const form = document.getElementById("form");
 form.addEventListener("submit", submit);
-
-
 
 const checkboxies = document.querySelector(".form__addservice-checkbox");
 function toggleAddService(){
@@ -17,13 +22,18 @@ addService.addEventListener("click", toggleAddService);
 */
 const hiddenMenu = document.querySelector(".banner__menu-hidden");
 const burgerMenuOff = document.querySelector(".banner__hamburger");
+const hiddenMenuItem = document.querySelectorAll(".banner__menu-hidden-item")
 function toggleHiddenMenu (){
-    hiddenMenu.classList.toggle("show")
+    hiddenMenu.style.display = "flex"
     burgerMenuOff.style.display = "none"
 }
 function hiddenMenuOff() {
     burgerMenuOff.style.display = "block"
     hiddenMenu.style.display = "none"
-
 }
-
+hiddenMenuItem.addEventListener("mouseover", ()=> {
+    for (let i=0; i<hiddenMenuItem.length; i++) {
+    hiddenMenuItem[i].style.background = "black"
+    hiddenMenuItem[i].style.color = "white"
+    }
+})
